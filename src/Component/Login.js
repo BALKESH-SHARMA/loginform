a8ee8ab60f247c890eb14ba030d15ab08deb8e6e
BALKESH-SHARMA <sharmabalkesh007@gmail.com>
2024-05-01 11:25:13 +0530

    initial commit

---
+++ src/Component/Login.js
@@ -0,0 +1,108 @@
+import * as React from 'react';
+
+import Button from '@mui/material/Button';
+
+import TextField from '@mui/material/TextField';
+
+import Link from '@mui/material/Link';
+import Paper from '@mui/material/Paper';
+import Box from '@mui/material/Box';
+import Grid from '@mui/material/Grid';
+//import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
+import Typography from '@mui/material/Typography';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+
+
+
+
+const defaultTheme = createTheme();
+
+export default function Login() {
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        const data = new FormData(event.currentTarget);
+        console.log({
+            email: data.get('email'),
+            password: data.get('password'),
+        });
+    };
+
+    return (
+        // <ThemeProvider theme={defaultTheme}>
+        <Box sx={{ height: '100vh', width: '100vw', display: "flex" }}>
+            <Grid container component="main" spacing={0} >
+                <Grid
+                    item
+                    xs={0}
+                    md={6}
+                    justifyContent="end" alignItems="center"
+                    display={"flex"}
+                    component={Paper}
+                    elevation={0}
+                >
+                    <Box sx={{ background: `radial-gradient(circle at center, #ffffff 50%, #e9f0f8 100%)`, height: '90%', width: '95%', borderRadius: '24px' }}>
+
+                    </Box>
+                </Grid>
+                <Grid item xs={12} md={6} component={Paper} square elevation={0} justifyContent="center" alignItems="center" display={"flex"}>
+                    <Box
+                        sx={{
+                            my: 8,
+                            mx: 4,
+                            // display: 'flex',
+                            // flexDirection: 'column',
+                            alignItems: 'start',
+                            width: '55%'
+                        }}
+                    >
+                        <Typography component="h1" variant="h5" fontWeight="bold">
+                            Welcome!
+                        </Typography>
+                        <Typography component="h5" paddingBottom="25px">
+                            It's great to see you here again.
+                        </Typography>
+                        <Box component="form" noValidate onSubmit={handleSubmit} sx={{ mt: 1 }}>
+                            <Typography>Email ID</Typography>
+                            <TextField
+                                margin="normal"
+                                required
+                                fullWidth
+                                id="email"
+
+                                name="email"
+                                placeholder='Enter email'
+                                autoComplete="email"
+                                autoFocus
+                            />
+                            <Typography>Password</Typography>
+                            <TextField
+                                placeholder='Enter password'
+                                margin="normal"
+                                required
+                                fullWidth
+                                name="password"
+                                //label="Password"
+                                type="password"
+                                id="password"
+                                autoComplete="current-password"
+                            />
+                            <Button
+                                type="submit"
+                                fullWidth
+                                variant="contained"
+                                sx={{ mt: 3, mb: 3, textTransform: 'none', height: "40px" }}
+                            >Login</Button>
+                            <Typography>
+                                Don't have an account?
+                                <Link href="#" variant="body2"  >
+                                    {"Sign up"}
+                                </Link>
+                            </Typography>
+                        </Box>
+                    </Box>
+                </Grid>
+            </Grid>
+        </Box>
+        // </ThemeProvider>
+    );
+}
